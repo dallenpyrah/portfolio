5ddfe481f1066a8bf540a3a754f6abec776bc256
Dallen Pyrah <dallenpyrah@gmail.com>
2021-04-28 18:10:42 -0600

    Changed public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  publicPath: '/portfolio/',
   outputDir: 'docs',
   productionSourceMap: false
 }
