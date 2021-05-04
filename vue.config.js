962cbd4b67bae7ebb017d87c931cbee2b81bdeb1
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-04 10:48:00 -0600

    okay

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/portfolio/',
-  outputDir: 'dist',
-  productionSourceMap: false
+  publicPath: '/',
+  outputDir: 'root',
+  productionSourceMap: false,
+  devServer: { https: false }
 }
