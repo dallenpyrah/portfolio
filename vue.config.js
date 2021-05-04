84392d544a29a1f79658cf8e0cf3fb601cdee41a
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-04 10:42:13 -0600

    sorry for so many commits

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/portfolio/',
-  outputDir: 'root',
+  outputDir: 'dist',
   productionSourceMap: false
 }
